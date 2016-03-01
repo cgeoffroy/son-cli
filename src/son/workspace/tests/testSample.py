21bb8378a500290bb468586c603dba894bf7b702
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-03-01 16:52:35 +0100

    Fix the dummy tests

--- src/son/workspace/tests/testSample.py
+++ src/son/workspace/tests/testSample.py
@@ -3,11 +3,11 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
 class MyTestCase2(unittest.TestCase):
     def test_something2(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
